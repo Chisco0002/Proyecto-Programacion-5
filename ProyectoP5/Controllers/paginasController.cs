73457b96a8c49db6339443aab49f814e7335f01e
mtlv99 <marco.tulio.leon@gmail.com>
2019-04-27 19:38:52 -0600

    Revert "Dash"
    This reverts commit 7dc32905aa0fa1101308c5f4ad0a4886d7c525c4, reversing
    changes made to 50c62ec260b6c049a84f26d5cdacdb3e0f98bf83.

--- ProyectoP5/Controllers/paginasController.cs
+++ ProyectoP5/Controllers/paginasController.cs
@@ -36,108 +36,102 @@ namespace ProyectoP5.Controllers
         {
             return View();
         }
-		Clases.correosControl indi = new Clases.correosControl();
-		public ActionResult enviarCorreos()
-		{
-			indi.Page_Load();
-			return View("informacionGeneral");
-		}
 
-		protected void usuariosConeccion(string nombreUsuario, string cedulaUsuario,
-										int edadUsuario, string correoUsuario,
-										string profesiónUsuario, string provinciaUsuario,
-										string cantonUsuario, string distritoUsuario)
-		{
+        protected void usuariosConeccion(string nombreUsuario, string cedulaUsuario,
+                                        int edadUsuario, string correoUsuario,
+                                        string profesiónUsuario, string provinciaUsuario,
+                                        string cantonUsuario, string distritoUsuario)
+        {
 
 
-			SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=proyectoProgramacionV;Integrated Security=True");
-			SqlCommand cmd = new SqlCommand("insercion_Usuario", con);
-			cmd.CommandType = CommandType.StoredProcedure;
-			cmd.Parameters.AddWithValue("nombreUsuario", nombreUsuario);
-			cmd.Parameters.AddWithValue("cedulaUsuario", cedulaUsuario);
-			cmd.Parameters.AddWithValue("edadUsuario", edadUsuario);
-			cmd.Parameters.AddWithValue("correoUsuario", correoUsuario);
-			cmd.Parameters.AddWithValue("profesiónUsuario", profesiónUsuario);
-			cmd.Parameters.AddWithValue("provinciaUsuario", provinciaUsuario);
-			cmd.Parameters.AddWithValue("cantonUsuario", cantonUsuario);
-			cmd.Parameters.AddWithValue("distritoUsuario", distritoUsuario);
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=proyectoProgramacionV;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insercion_Usuario", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("nombreUsuario", nombreUsuario);
+            cmd.Parameters.AddWithValue("cedulaUsuario", cedulaUsuario);
+            cmd.Parameters.AddWithValue("edadUsuario", edadUsuario);
+            cmd.Parameters.AddWithValue("correoUsuario", correoUsuario);
+            cmd.Parameters.AddWithValue("profesiónUsuario", profesiónUsuario);
+            cmd.Parameters.AddWithValue("provinciaUsuario", provinciaUsuario);
+            cmd.Parameters.AddWithValue("cantonUsuario", cantonUsuario);
+            cmd.Parameters.AddWithValue("distritoUsuario", distritoUsuario);
 
 
-			con.Open();
-			int k = cmd.ExecuteNonQuery();
-			if (k != 0)
-			{
-			}
-			con.Close();
-		}
+            con.Open();
+            int k = cmd.ExecuteNonQuery();
+            if (k != 0)
+            {
+            }
+            con.Close();
+        }
 
-		public ActionResult formularioRecive(FormCollection form)
-		{ //desde esta accion se procedera a realizar el ingrso a la base de datos
+        public ActionResult formularioRecive(FormCollection form)
+        { //desde esta accion se procedera a realizar el ingrso a la base de datos
 
-			var nombre = form["txtNombre"];
-			var cedula = form["txtCedula"];
-			var edad = form["txtEdad"];
-			var correo = form["txtCorreo"];
-			var profecion = form["txtProfecion"];
-			var provincia = form["Provincia"];
-			var canton = form["txtCanton"];
-			var distrito = form["txtDistrito"];
+            var nombre = form["txtNombre"];
+            var cedula = form["txtCedula"];
+            var edad = form["txtEdad"];
+            var correo = form["txtCorreo"];
+            var profecion = form["txtProfecion"];
+            var provincia = form["Provincia"];
+            var canton = form["txtCanton"];
+            var distrito = form["txtDistrito"];
 
-			ViewBag.Provincia = provincia;
-			ViewBag.Nombre = nombre;
-			ViewBag.cedula = cedula;
-			ViewBag.edad = edad;
-			ViewBag.Correo = correo;
-			ViewBag.profecion = profecion;
-			ViewBag.canton = canton;
-			ViewBag.distrito = distrito;
+            ViewBag.Provincia = provincia;
+            ViewBag.Nombre = nombre;
+            ViewBag.cedula = cedula;
+            ViewBag.edad = edad;
+            ViewBag.Correo = correo;
+            ViewBag.profecion = profecion;
+            ViewBag.canton = canton;
+            ViewBag.distrito = distrito;
 
-			if (int.Parse(ViewBag.Provincia) == 0)
-			{
-				ViewBag.Provincia = "San Jose";
-			}
-			else if (int.Parse(ViewBag.Provincia) == 1)
-			{
-				ViewBag.Provincia = "Alajuelita";
-			}
-			else if (int.Parse(ViewBag.Provincia) == 2)
-			{
-				ViewBag.Provincia = "Guanacaste";
-			}
-			else if (int.Parse(ViewBag.Provincia) == 3)
-			{
-				ViewBag.Provincia = "Puntarenas";
-			}
-			else if (int.Parse(ViewBag.Provincia) == 4)
-			{
-				ViewBag.Provincia = "Limon";
-			}
-			else if (int.Parse(ViewBag.Provincia) == 5)
-			{
-				ViewBag.Provincia = "Heredia";
-			}
-			else
-			{
-				ViewBag.Provincia = "Cartago";
-			}
-			int edadNumero = Int32.Parse(edad);
+            if (int.Parse(ViewBag.Provincia) == 0)
+            {
+                ViewBag.Provincia = "San Jose";
+            }
+            else if (int.Parse(ViewBag.Provincia) == 1)
+            {
+                ViewBag.Provincia = "Alajuela";
+            }
+            else if (int.Parse(ViewBag.Provincia) == 2)
+            {
+                ViewBag.Provincia = "Guanacaste";
+            }
+            else if (int.Parse(ViewBag.Provincia) == 3)
+            {
+                ViewBag.Provincia = "Puntarenas";
+            }
+            else if (int.Parse(ViewBag.Provincia) == 4)
+            {
+                ViewBag.Provincia = "Limon";
+            }
+            else if (int.Parse(ViewBag.Provincia) == 5)
+            {
+                ViewBag.Provincia = "Heredia";
+            }
+            else
+            {
+                ViewBag.Provincia = "Cartago";
+            }
+            int edadNumero = Int32.Parse(edad);
 
-			try
-			{
-				usuariosConeccion(nombre, cedula, edadNumero, correo, profecion, provincia, canton, distrito);
+            try
+            {
+                usuariosConeccion(nombre, cedula, edadNumero, correo, profecion, provincia, canton, distrito);
 
-				return View("confirmacionFormulario");
-			}
-			catch (Exception)
-			{
-				return View("error");
-				throw;
-			}
+                return View("confirmacionFormulario");
+            }
+            catch (Exception)
+            {
+                return View("error");
+                throw;
+            }
 
 
 
-		}
-		public ActionResult foro()
+        }
+        public ActionResult foro()
         {
             return View();
         }
